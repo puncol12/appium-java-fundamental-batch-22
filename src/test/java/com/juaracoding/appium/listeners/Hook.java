1ebb38ca2e5551157291f2c7acf3307b8b426dc9
[yusuf nur rizqi] <yn.yusufnur@gmail.com>
2025-05-28 22:05:11 +0700

    dooonee

--- src/test/java/com/juaracoding/appium/listeners/Hook.java
+++ src/test/java/com/juaracoding/appium/listeners/Hook.java
@@ -1,11 +1,10 @@
 package com.juaracoding.appium.listeners;
 
 import java.net.MalformedURLException;
-import java.nio.charset.MalformedInputException;
 import java.time.Duration;
 // import org.openqa.selenium.WebDriver;
 import org.testng.IExecutionListener;
-import com.juaracoding.appium.AndroidDeviceManager;
+
 import com.juaracoding.appium.DriverSingleton;
 import com.juaracoding.appium.utils.MailSender;
 import io.appium.java_client.android.AndroidDriver;
@@ -15,19 +14,19 @@ import io.appium.java_client.android.AndroidDriver;
 public class Hook implements IExecutionListener {
     public static AndroidDriver driver;
 
-  @Override
-  public void onExecutionStart() {
-    System.err.println("TestNG is commencing execution");
-    try {
-     driver = DriverSingleton.createOrGetDriver();
-     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-     driver.hideKeyboard();   
-    } catch (MalformedURLException e) {
-       
-        e.printStackTrace();
+    @Override
+    public void onExecutionStart() {
+        System.err.println("TestNG is commencing execution");
+        try {
+            driver = DriverSingleton.createOrGetDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+            driver.hideKeyboard();
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+        }
+
     }
-    
-  }
 
     @Override
     public void onExecutionFinish() {
